d2e747c85610b422e348547249721ddfc7d31c45
Your Name <you@example.com>
2024-05-09 15:07:22 +0530

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -11,6 +11,7 @@ export default clerkMiddleware((auth, req) => {
   if (!auth().userId && isProtectedRoute(req)) {
  
     // Add custom logic to run before redirecting
+    publicRoutes: ['/api/webhooks/clerk']
  
     return auth().redirectToSignIn();
   }
